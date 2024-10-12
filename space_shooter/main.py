466fe831def763cf353df0469fbcbf977c746375
Zykie <zykie1018@gmail.com>
2024-10-12 22:14:52 +0800

    feat: add random placement of stars

--- space_shooter/main.py
+++ space_shooter/main.py
@@ -1,5 +1,5 @@
 import os
-
+import random
 import pygame
 from constants import WINDOW_HEIGHT, WINDOW_WIDTH
 
@@ -14,15 +14,24 @@ surface.fill("seagreen")
 
 
 player_surface = pygame.image.load(os.path.join("images", "player.png")).convert_alpha()
-
+star_surface = pygame.image.load(os.path.join("images", "star.png")).convert_alpha()
+star_positions = [
+    (random.randint(0, WINDOW_WIDTH), random.randint(0, WINDOW_HEIGHT))
+    for _ in range(20)
+]
 while running:
     # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    display_surface.fill("darkseagreen1")
+    display_surface.fill("darkgray")
+
+    for position in star_positions:
+        display_surface.blit(star_surface, position)
+
     display_surface.blit(player_surface, (100, 150))
+
     pygame.display.update()
 
 pygame.quit()
