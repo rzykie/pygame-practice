0a55de9a504fe727b8bd985381bfa2037862de11
Zykie <zykie1018@gmail.com>
2024-10-12 16:45:19 +0800

    feat: add surface

--- main.py
+++ main.py
@@ -6,13 +6,18 @@ pygame.init()
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("space shooter")
 running = True
-while True:
+
+surface = pygame.Surface((100, 200))
+surface.fill("seagreen")
+
+while running:
     # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     display_surface.fill("darkseagreen1")
+    display_surface.blit(surface, (100, 150))
     pygame.display.update()
 
 pygame.quit()
