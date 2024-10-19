2e65ddcce94f75bafa8d3ea6b949befad6ba07fd
Zykie Rallos <zykie1018@gmail.com>
2024-10-19 13:58:53 +0800

    Feature: practice with vector2

--- space_shooter/main.py
+++ space_shooter/main.py
@@ -8,6 +8,7 @@ pygame.init()
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("space shooter")
 running = True
+clock = pygame.time.Clock()
 
 surface = pygame.Surface((100, 200))
 surface.fill("seagreen")
@@ -15,8 +16,8 @@ surface.fill("seagreen")
 
 player_surface = pygame.image.load(os.path.join("images", "player.png")).convert_alpha()
 player_frect = player_surface.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-player_direction = -1
-
+player_direction = pygame.math.Vector2(2, -1)
+player_speed = 10
 
 star_surface = pygame.image.load(os.path.join("images", "star.png")).convert_alpha()
 star_positions = [
@@ -31,6 +32,7 @@ laser_surface = pygame.image.load(os.path.join("images", "laser.png")).convert_a
 laser_frect = laser_surface.get_frect(bottomleft=(20, WINDOW_HEIGHT - 20))
 
 while running:
+    clock.tick(10)
     # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -41,15 +43,16 @@ while running:
     for position in star_positions:
         display_surface.blit(star_surface, position)
 
-    player_frect.x += player_direction * 0.8
-    if player_frect.right > WINDOW_WIDTH or player_frect.left < 0:
-        player_direction *= -1
-
-    display_surface.blit(player_surface, player_frect)
-
     display_surface.blit(meteor_surface, meteor_frect)
 
     display_surface.blit(laser_surface, laser_frect)
+
+    player_frect.center += player_direction * player_speed
+
+    # if player_frect.right > WINDOW_WIDTH or player_frect.left < 0:
+    #     player_direction *= -1
+
+    display_surface.blit(player_surface, player_frect)
 
     pygame.display.update()
 
