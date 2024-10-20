5c279702c3541df26f2299bea66d456766e68a3e
Zykie Rallos <zykie1018@gmail.com>
2024-10-20 18:26:57 +0800

    Feature: add random meteor objects to show up within the window

--- space_shooter/main.py
+++ space_shooter/main.py
@@ -14,6 +14,11 @@ class Player(pygame.sprite.Sprite):
         self.direction = pygame.math.Vector2()
         self.speed = 300
 
+        # cooldown
+        self.can_shoot = True
+        self.laser_shoot_time = 0
+        self.cooldown_duration = 200
+
     def update(self, delta_time):
         keys = pygame.key.get_pressed()
 
@@ -26,8 +31,18 @@ class Player(pygame.sprite.Sprite):
 
         recent_key = pygame.key.get_just_pressed()
 
-        if recent_key[pygame.K_SPACE]:
-            print("fire laser")
+        if recent_key[pygame.K_SPACE] and self.can_shoot:
+            Laser(all_sprites, laser_surface, self.rect.midtop)
+            self.can_shoot = False
+            self.laser_shoot_time = pygame.time.get_ticks()
+
+        self.laser_timer()
+
+    def laser_timer(self):
+        if not self.can_shoot:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.laser_shoot_time >= self.cooldown_duration:
+                self.can_shoot = True
 
 
 class Star(pygame.sprite.Sprite):
@@ -39,33 +54,52 @@ class Star(pygame.sprite.Sprite):
         )
 
 
-pygame.init()
+class Laser(pygame.sprite.Sprite):
+    def __init__(self, groups, surface, position):
+        super().__init__(groups)
+        self.image = surface
+        self.rect = self.image.get_frect(midbottom=position)
 
+    def update(self, delta_time):
+        self.rect.centery -= 400 * delta_time
+        if self.rect.bottom < 0:
+            self.kill()
+
+
+class Meteor(pygame.sprite.Sprite):
+    def __init__(self, groups, surface, position):
+        super().__init__(groups)
+        self.image = surface
+        self.rect = self.image.get_frect(center=position)
+
+        self.meteor_time = 0
+
+    def meteor_timer(self):
+        current_time = pygame.time.get_ticks()
+        if current_time - self.laser_shoot_time >= self.cooldown_duration:
+            self.can_shoot = True
+
+
+# General setup
+pygame.init()
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("space shooter")
 running = True
 clock = pygame.time.Clock()
 
-surface = pygame.Surface((100, 200))
-surface.fill("seagreen")
-
+# import section
 all_sprites = pygame.sprite.Group()
-
 star_surface = pygame.image.load(os.path.join("images", "star.png")).convert_alpha()
 for i in range(20):
     Star(all_sprites, star_surface)
+laser_surface = pygame.image.load(os.path.join("images", "laser.png")).convert_alpha()
+meteor_surface = pygame.image.load(os.path.join("images", "meteor.png")).convert_alpha()
 player = Player(all_sprites)
 
 
-meteor_surface = pygame.image.load(os.path.join("images", "meteor.png")).convert_alpha()
-meteor_frect = meteor_surface.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-
-laser_surface = pygame.image.load(os.path.join("images", "laser.png")).convert_alpha()
-laser_frect = laser_surface.get_frect(bottomleft=(20, WINDOW_HEIGHT - 20))
-
 # custom event -> meteor event
 meteor_event = pygame.event.custom_type()
-pygame.time.set_timer(meteor_event, 500)
+pygame.time.set_timer(meteor_event, 1000)
 while running:
     delta_time = clock.tick(60) / 1000
     # event loop
@@ -73,11 +107,11 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == meteor_event:
-            print("create meteor")
-        # if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-        #     print(1)
-        # if event.type == pygame.MOUSEMOTION:
-        #     player_frect.center = event.pos
+            meteor_position_1, meteor_position_2 = random.randint(
+                0, WINDOW_WIDTH
+            ), random.randint(0, WINDOW_HEIGHT)
+            Meteor(all_sprites, meteor_surface, (meteor_position_1, meteor_position_2))
+
     all_sprites.update(delta_time)
 
     display_surface.fill("darkgray")
