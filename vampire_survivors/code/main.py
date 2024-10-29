73da8e4b5d8ca3438f73e3a46ef6195352ba0e96
Zykie Rallos <zykie1018@gmail.com>
2024-10-29 22:23:40 +0800

    Feature: add initial setup for game loop and player class

--- vampire_survivors/code/main.py
+++ vampire_survivors/code/main.py
@@ -1,1 +1,42 @@
-from settings import *
+import os
+
+import pygame
+from settings import TILE_SIZE, WINDOW_HEIGHT, WINDOW_WIDTH
+
+
+class Game:
+    def __init__(self):
+        super().__init__()
+        self.startup = pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.title_name = "vampire_survivors"
+        self.running = True
+        self.clock = pygame.time.Clock()
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
+        self.original_surface = pygame.image.load(
+            os.path.join("images", "player.png")
+        ).convert_alpha()
+        self.image = self.original_surface
+        self.rect = self.image.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+        self.direction = pygame.math.Vector2()
+        self.speed = 800
+
+
+game = Game()
+
+while game.running:
+    delta_time = game.clock.tick(60) / 1000
+    # event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game.running = False
+    game.display_surface.fill("#6415B8")
+
+    pygame.display.update()
+
+
+pygame.quit()
--- vampire_survivors/code/settings.py
+++ vampire_survivors/code/settings.py
@@ -1,6 +1,7 @@
-import pygame 
-from os.path import join 
+import os
 from os import walk
 
-WINDOW_WIDTH, WINDOW_HEIGHT = 1280,720 
+import pygame
+
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 TILE_SIZE = 64
